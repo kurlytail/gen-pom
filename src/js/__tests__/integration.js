1e4a12872a2481c9fbf2558e24ca25c45226e1c3
ashwin <ashwinkn@hotmail.com>
2019-03-18 23:36:55 -0500

    Upgraded lib

--- src/js/__tests__/integration.js
+++ src/js/__tests__/integration.js
@@ -6,24 +6,15 @@ describe('# integration test', () => {
     });
 
     it('## should print help options', () => {
-        const output = execSync('./scripts/sgen-pom.sh -h').toString();
-        expect(output).toMatchSnapshot();
-    });
-
-    it('## should generate design', () => {
-        const output = execSync('./scripts/sgen-pom.sh -d src/test/fixture/design.json -o testoutput').toString();
-        expect(output).toMatchSnapshot();
-    });
-
-    it('## should generate design with merge', () => {
-        let output = execSync('./scripts/sgen-pom.sh -d src/test/fixture/design.json -o testoutput').toString();
-        expect(output).toMatchSnapshot();
-        output = execSync('./scripts/sgen-pom.sh -d src/test/fixture/design.json -o testoutput').toString();
+        let output = execSync('npm run build').toString();
+        output = execSync('sgen -g `pwd`/dist/pom.min.js -h').toString();
         expect(output).toMatchSnapshot();
     });
 
     it('## should generate design and run pom commands', () => {
-        let output = execSync('./scripts/sgen-pom.sh -d src/test/fixture/design.json -o testoutput').toString();
+        let output = execSync('npm run build').toString();
+        output = execSync('sgen -g `pwd`/dist/pom.min.js -d src/test/fixture/design.json -o testoutput').toString();
+        output = output.replace(/info: Loaded generator .*pom.min.js.*/, '');
         expect(output).toMatchSnapshot();
         output = execSync('npm install', { cwd: 'testoutput' }).toString();
         output = execSync('npm run lint', { cwd: 'testoutput' }).toString();
@@ -32,7 +23,7 @@ describe('# integration test', () => {
 
     it('## should generate design and run pom commands with reactor extensions', () => {
         let output = execSync(
-            './scripts/sgen-pom.sh -e templates/addons/reactor -d src/test/fixture/design.json -o testoutput'
+            'sgen -g `pwd`/dist/pom.min.js -e templates/addons/reactor -d src/test/fixture/design.json -o testoutput'
         ).toString();
         expect(output).toMatchSnapshot();
         output = execSync('npm install', { cwd: 'testoutput' }).toString();
